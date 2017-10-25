b79a1f674b5c5c3a116304a5deb5293e908fc994
Nathan Rahm <rahm.nathan@gmail.com>
2017-10-24 20:12:56 -0400

    Added Spring-Boot Camel route for push notifications

---
+++ google-pushnotification/src/main/java/com/github/rahmnathan/google/pushnotification/config/CamelPushNotificationConfig.java
@@ -0,0 +1,48 @@
+package com.github.rahmnathan.google.pushnotification.config;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import java.util.logging.Logger;
+
+@Component
+public class CamelPushNotificationConfig {
+
+    @Value(value = "${push.notification.key}")
+    private String serverKey;
+    private final Logger logger = Logger.getLogger(CamelPushNotificationConfig.class.getName());
+    private final CamelContext camelContext;
+
+    @Inject
+    public CamelPushNotificationConfig(CamelContext camelContext){
+        this.camelContext = camelContext;
+    }
+
+    @PostConstruct
+    public void configureCamelRoutes(){
+        try {
+            camelContext.addRoutes(new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+                    onException(Exception.class)
+                            .useExponentialBackOff()
+                            .redeliveryDelay(2000)
+                            .maximumRedeliveries(5);
+
+                    from("seda:pushnotification")
+                            .marshal().json(JsonLibrary.Jackson)
+                            .setHeader("Authorization", constant("key=" + serverKey))
+                            .setHeader("Content-Type", constant("application/json"))
+                            .to("http4://fcm.googleapis.com/fcm/send");
+                }
+            });
+        } catch (Exception e){
+            logger.severe(e.toString());
+        }
+    }
+}
