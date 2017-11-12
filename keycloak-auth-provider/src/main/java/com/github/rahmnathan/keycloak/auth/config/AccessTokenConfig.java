a67c16d2a905e1db148c3038a162a63f3873b9ea
nathan <rahm.nathan@gmail.com>
2017-11-12 16:43:10 -0600

    Added Keycloak adapter

---
+++ keycloak-auth-provider/src/main/java/com/github/rahmnathan/keycloak/auth/config/AccessTokenConfig.java
@@ -0,0 +1,44 @@
+package com.github.rahmnathan.keycloak.auth.config;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.ws.rs.HttpMethod;
+import java.util.logging.Logger;
+
+@Component
+public class AccessTokenConfig {
+    private final Logger logger = Logger.getLogger(AccessTokenConfig.class.getName());
+    private final CamelContext camelContext;
+
+    @Autowired
+    public AccessTokenConfig(CamelContext camelContext){
+        this.camelContext = camelContext;
+    }
+
+    @PostConstruct
+    public void initialize(){
+        try {
+            camelContext.addRoutes(new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+                    onException(Exception.class)
+                            .useExponentialBackOff()
+                            .redeliveryDelay(1000)
+                            .maximumRedeliveries(5);
+
+                    from("direct:accesstoken")
+                            .setHeader(Exchange.HTTP_METHOD, constant(HttpMethod.POST))
+                            .setHeader(Exchange.CONTENT_TYPE, constant("application/x-www-form-urlencoded"))
+                            .to("https4://localmovies-cloud.hopto.org/auth/realms/LocalMovies/protocol/openid-connect/token");
+                }
+            });
+        } catch (Exception e){
+            logger.severe(e.toString());
+        }
+    }
+}
---
+++ keycloak-auth-provider/src/main/java/com/github/rahmnathan/keycloak/auth/utils/KeycloakUtils.java
@@ -0,0 +1,47 @@
+package com.github.rahmnathan.keycloak.auth.utils;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.ProducerTemplate;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+public class KeycloakUtils {
+    private static final Logger logger = Logger.getLogger(KeycloakUtils.class.getName());
+
+    public static String getAccessToken(String username, String password, ProducerTemplate producerTemplate) {
+        String requestBody = buildLoginInfo(username, password);
+        Map<String, Object> headers = Map.of(Exchange.CONTENT_LENGTH, requestBody.getBytes().length);
+
+        String response = producerTemplate.requestBodyAndHeaders("direct:accesstoken", requestBody, headers, String.class);
+
+        JSONObject jsonObject = new JSONObject(response);
+        if(jsonObject.has("access_token"))
+            return jsonObject.getString("access_token");
+
+        throw new RuntimeException("Failed to get access token.");
+    }
+
+    private static String buildLoginInfo(String username, String password) {
+        Map<String, String> args = new HashMap<>();
+        args.put("grant_type", "password");
+        args.put("client_id", "movielogin");
+        args.put("username", username);
+        args.put("password", password);
+        StringBuilder sb = new StringBuilder();
+        args.forEach((key, value) -> {
+            try {
+                sb.append(URLEncoder.encode(key, "UTF-8")).append("=")
+                        .append(URLEncoder.encode(value, "UTF-8")).append("&");
+            } catch (UnsupportedEncodingException e) {
+                logger.severe(e.toString());
+            }
+        });
+
+        return sb.toString().substring(0, sb.length() - 1);
+    }
+}
