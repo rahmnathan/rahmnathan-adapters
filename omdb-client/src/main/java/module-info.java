07c76effd9c6b906e0b5e5f3206d9ea65d746b91
nathan <rahm.nathan@gmail.com>
2018-09-21 21:20:22 -0500

    Migrated to Java 10.

---
+++ omdb-client/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module movie.info.omdb {
+    requires slf4j.api;
+    requires camel.core;
+    requires camel.http.common;
+    requires com.fasterxml.jackson.databind;
+    requires camel.http4;
+}
